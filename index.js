26e7f131cbc26833ef25320685a0c3da1f0c970e
ashu <ashuchoudhury22@gmail.com>
2024-11-25 19:46:48 +0530

    help root added

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const app=express();
 app.use(express.json());
 
 app.use("/ai", AI);
-
+app.get("/help", (req, res) => {
+   res.json({
+msg: "hello server is Very happy ",
+       time: new Date().toLocaleString()
+});
+});
 app.listen(2000, () => {
     console.log("server is up!");
 });
